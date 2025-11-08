1f06bfbe932f982a132fd6ba8721af0a7c361eb4
HaleyMaurice <riggersphinney@gmail.com>
2025-11-08 11:42:53 +0000

    Update main page layout styling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import { ResumeMatchDemo } from "@/components/ResumeMatchDemo";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="min-h-screen">
       <div className="flex flex-col gap-8 items-center sm:items-start w-full px-3 md:px-0">
         <ResumeMatchDemo />
       </div>
